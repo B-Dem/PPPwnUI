2256cc167e5d53f0d00bb01e7c2d2d6bdd5884f1
B-Dem <158171473+B-Dem@users.noreply.github.com>
2024-05-08 00:13:01 +0200

    Update V2.0
    Updated to lastest version of PPPwn

--- PPPwnUI.py
+++ PPPwnUI.py
@@ -1,7 +1,9 @@
 import tkinter as tk
-from tkinter import messagebox, scrolledtext
+from tkinter import messagebox, filedialog
 import psutil
 import subprocess
+import os
+import sys
 
 def get_network_interface_names():
     interfaces = psutil.net_if_addrs()
@@ -9,14 +11,19 @@ def get_network_interface_names():
 
 class App:
     def __init__(self, master):
+
+        if sys.platform == "linux" and not os.geteuid() == 0:
+            print("You must run this program as administrator.")
+            sys.exit(1)
+
         self.master = master
-        master.title("PPwnUI v1.1 By Memz")
+        master.title("PPwnUI v2.0 By Memz")
 
-        # Définir la taille de la fenêtre
-        master.geometry("450x450")
+        # taille de la fenêtre
+        master.geometry("400x500")
 
-        # Ajouter un logo d'application
-        master.iconbitmap("media/sighya.ico")
+        # logo d'application
+        master.iconbitmap("media/logo.ico")
 
         self.menu = tk.Menu(master)
         master.config(menu=self.menu)
@@ -42,47 +49,98 @@ class App:
         self.interface_menu = tk.OptionMenu(master, self.interface_var, *get_network_interface_names())
         self.interface_menu.pack()
 
-        # Ajouter les firmwares
-        self.firmware_var = tk.StringVar(master)
-        self.firmware_var.set("900")  # Firmware pré-sélectionné
+        # Conteneur pour les colonnes des firmwares
         self.firmware_label = tk.Label(master, text="Choose your Firmware:")
         self.firmware_label.pack()
-        self.firmware_radio_900 = tk.Radiobutton(master, text="9.00", variable=self.firmware_var, value="900")
-        self.firmware_radio_903 = tk.Radiobutton(master, text="9.03", variable=self.firmware_var, value="903")
-        self.firmware_radio_904 = tk.Radiobutton(master, text="9.04", variable=self.firmware_var, value="904")
-        self.firmware_radio_1100 = tk.Radiobutton(master, text="11.00", variable=self.firmware_var, value="1100")
-        self.firmware_radio_1000 = tk.Radiobutton(master, text="10.00", variable=self.firmware_var, value="1000")
-        self.firmware_radio_1001 = tk.Radiobutton(master, text="10.01", variable=self.firmware_var, value="1001")
-        self.firmware_radio_900.pack()
-        self.firmware_radio_903.pack()
-        self.firmware_radio_904.pack()
-        self.firmware_radio_1000.pack()
-        self.firmware_radio_1001.pack()
-        self.firmware_radio_1100.pack()
+        self.columns_container = tk.Frame(master)
+        self.columns_container.pack()
 
-        # Bouton Start PPPwn
+        # Firmwares avec noms des versions
+        firmware_versions = ["7.50", "7.51", "7.55", "8.00", "8.01", "8.03", "9.00", "9.03", "9.04", "9.50", "9.51", "9.60", "10.00", "10.01", "11.00", "Custom"]
+
+        self.firmware_var = tk.StringVar(master)
+        self.firmware_var.set("7.50")  # Firmware pré-sélectionné
+
+        # Création boutons radio pour chaque firmware dans les colonnes gauche & droite
+        self.left_column = tk.Frame(self.columns_container)
+        self.left_column.pack(side=tk.LEFT)
+
+        self.right_column = tk.Frame(self.columns_container)
+        self.right_column.pack(side=tk.RIGHT)
+
+        column_counter = 0
+
+        for firmware in firmware_versions:
+            radio_button = tk.Radiobutton(self.left_column if column_counter % 2 == 0 else self.right_column, text=firmware, variable=self.firmware_var, value=firmware, command=self.show_payload_options if firmware == "Custom" else None)
+            radio_button.pack(anchor=tk.W)
+            column_counter += 1
+
+        # Sélection payloads
+        self.payload_frame = tk.Frame(master)
+
+        self.payload_label = tk.Label(self.payload_frame, text="Select Payloads:")
+        self.payload_label.pack()
+
+        self.payload_var = tk.StringVar(master)
+
+        self.custom_payloads_frame = tk.Frame(master)
+
+        self.stage1_label = tk.Label(self.custom_payloads_frame, text="Custom Stage 1:")
+        self.stage1_label.grid(row=0, column=0)
+
+        self.stage1_path = tk.StringVar()
+        self.stage1_entry = tk.Entry(self.custom_payloads_frame, textvariable=self.stage1_path, width=30)
+        self.stage1_entry.grid(row=0, column=1)
+
+        self.stage1_browse_button = tk.Button(self.custom_payloads_frame, text="Browse", command=self.select_stage1_file)
+        self.stage1_browse_button.grid(row=0, column=2, padx=5)
+
+        self.stage2_label = tk.Label(self.custom_payloads_frame, text="Custom Stage 2:")
+        self.stage2_label.grid(row=1, column=0)
+
+        self.stage2_path = tk.StringVar()
+        self.stage2_entry = tk.Entry(self.custom_payloads_frame, textvariable=self.stage2_path, width=30)
+        self.stage2_entry.grid(row=1, column=1)
+
+        self.stage2_browse_button = tk.Button(self.custom_payloads_frame, text="Browse", command=self.select_stage2_file)
+        self.stage2_browse_button.grid(row=1, column=2, padx=5)
+
+        # Start PPPwn
         self.start_button = tk.Button(master, text="Start PPPwn", command=self.start_pppwn)
         self.start_button.pack(side=tk.BOTTOM, pady=10)
+
+    def show_payload_options(self):
+        if self.firmware_var.get() == "Custom":
+            self.payload_frame.pack()
+            self.custom_payloads_frame.pack()
+        else:
+            self.payload_frame.pack_forget()
+            self.custom_payloads_frame.pack_forget()
+
+    def select_stage1_file(self):
+        stage1_file = filedialog.askopenfilename()
+        self.stage1_path.set(stage1_file)
+
+    def select_stage2_file(self):
+        stage2_file = filedialog.askopenfilename()
+        self.stage2_path.set(stage2_file)
 
     def start_pppwn(self):
         interface = self.interface_var.get()
         firmware = self.firmware_var.get()
 
-        if firmware == "900":
-            command = f'python pppwn900.py --interface="{interface}" --fw=900'
-        elif firmware == "1100":
-            command = f'python pppwn1100.py --interface="{interface}" --fw=1100'
-        elif firmware == "903":
-            command = f'python pppwn903.py --interface="{interface}" --fw=903'
-        elif firmware == "904":
-            command = f'python pppwn904.py --interface="{interface}" --fw=904'
-        elif firmware == "1000":
-            command = f'python pppwn1000.py --interface="{interface}" --fw=1000'
-        elif firmware == "1001":
-            command = f'python pppwn1001.py --interface="{interface}" --fw=1001'
+        stage1_path = self.stage1_path.get()
+        stage2_path = self.stage2_path.get()
+
+        if firmware == "Custom":
+            command = f'python PPPwn/pppwn.py --interface="{interface}" --stage1="{stage1_path}" --stage2="{stage2_path}"'
         else:
-            messagebox.showerror("Error", "Invalid firmware selection")
-            return
+            firmware_value = firmware.replace(".", "")
+            if firmware_value.isdigit():
+                command = f'python PPPwn/pppwn{firmware_value}.py --interface="{interface}" --fw={firmware_value}'
+            else:
+                messagebox.showerror("Error", "Invalid firmware selection")
+                return
 
         try:
             subprocess.Popen(command, shell=True)
@@ -90,7 +148,7 @@ class App:
             messagebox.showerror("Error", f"An error occurred: {e}")
 
     def about(self):
-        messagebox.showinfo("About", "PPPwnUI v1.1\nThis app was developped by Memz for Sighya to make PPPwn easier to use.")
+        messagebox.showinfo("About", "PPPwnUI v2.0\nThis app was developed by Memz to make PPPwn easier to use.")
 
 root = tk.Tk()
 app = App(root)
